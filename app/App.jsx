547885db9875185c58fa0d14313c6fd5f1d3a603
10000highfives <jared.a.fowler@gmail.com>
2016-03-31 23:15:19 -0700

    initial commit

---
+++ app/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import Logo from './components/Logo.jsx'
+import Button from './components/Button.jsx'
+import WebFlight from '../webflight/Webflight.jsx'
+import path from 'path'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {num: Math.floor(Math.random() * 4)}
+    this.refresh = function () {
+      return this.setState({num: Math.floor(Math.random() * 4)})
+    }.bind(this)
+  }
+
+  render () {
+    const logos = ['imgs/google.png', 'imgs/apple.png', 'imgs/netflix.png', 'imgs/spacex.png']
+
+    return (
+    <div>
+      <WebFlight src={logos[this.state.num]} />
+      <Button fn={this.refresh} />
+      <WebFlight src="imgs/spacex.png" style={{height: 200}} />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ app/components/Logo.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import wfReplace from '../utils/wf.js';
+
+class Logo extends Component {
+  constructor() {
+    super();
+
+    this.wfProps = {};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.wfProps = wfReplace(nextProps);
+  }
+
+  componentWillMount() {
+    // console.log('this is this.wfProps: ', this.wfProps)
+    this.wfProps = wfReplace(this.props);
+  }
+  // this.wfProps.[propName]
+  render() {
+    console.log('this is this.wfProps: ', this.wfProps)
+    return (
+      <div>
+        <img src={this.wfProps.image}/>
+      </div>
+    );
+  }
+}
+
+export default Logo;
