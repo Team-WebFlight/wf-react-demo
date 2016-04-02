6dad3527c90b611f20f65fcdf4697a5d38b62262
10000highfives <jared.a.fowler@gmail.com>
2016-04-02 07:55:30 -0700

    Fix typo, breaks line at 80char

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -4,7 +4,8 @@ import WebFlight from '../../webflight/WebFlight.jsx'
 class Gallery extends Component {
   render() {
     let images = this.props.source.map((img, i) => 
-      <WebFlight source={img} key={i} cls={this.props.cls} handle={this.props.handle} />)
+      <WebFlight source={img} key={i} cls={this.props.cls} 
+      handle={this.props.handle} />)
     return (
       <div>
        { images }
