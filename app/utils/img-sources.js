84227f607a553d3175069c6802b132d0711c2736
10000highfives <jared.a.fowler@gmail.com>
2016-04-06 16:05:58 -0700

    Rearranges src list for a better side-by-side appearance when imgs are rendered in masonry

--- app/utils/img-sources.js
+++ app/utils/img-sources.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sources: ['imgs/blue-1.jpg', 'imgs/blue-2.jpg', 'imgs/blue-3.jpg',
-    'imgs/blue-4.jpg', 'imgs/green-1.jpg', 'imgs/green-2.jpg',
+  sources: ['imgs/blue-2.jpg', 'imgs/blue-4.jpg','imgs/blue-1.jpg', 
+    'imgs/blue-3.jpg', 'imgs/green-1.jpg', 'imgs/green-2.jpg',
     'imgs/green-3.jpg', 'imgs/green-4.jpg', 'imgs/orange-1.jpg',
     'imgs/orange-3.jpg', 'imgs/orange-2.jpg', 'imgs/white-3.jpg',
     'imgs/yellow-1.jpg', 'imgs/yellow-2.jpg', 'imgs/yellow-3.jpg',
